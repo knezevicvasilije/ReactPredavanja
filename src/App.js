73a3d77546aebd9c0025fa1e99af9926e5c5c536
knezevicvasilije <73421159+knezevicvasilije@users.noreply.github.com>
2021-01-30 22:16:39 +0100

    prevState added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import { useState } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  const updateCounter = () => {
-    let a = counter + 1;
-    setCounter(a);
-  };
+
   console.log("rerendered");
   return (
     <div className="App">
       {counter}
-      <button onClick={() => updateCounter()}>Count +1</button>
+      <button onClick={() => setCounter((prevState) => prevState + 1)}>
+        Count +1
+      </button>
     </div>
   );
 }
